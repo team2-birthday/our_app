71530010aca1227a906d2119ac380142ecd19024
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2022-03-06 11:26:30 +0900

    登録ボタン作成

---
+++ src/components/RegisterCircle.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <button v-on:click="resisterCircle">登録</button>
+  </div>
+</template>
+
+<script>
+    import { setDoc, doc} from "firebase/firestore";
+    import { db } from "@/firebase.js";
+    export default{
+        data(){
+            return{
+                dates: ["3/10","3/11"],
+                number: 0,
+                places: ["公園","体育館"],
+                text: "aa",
+                name: "",
+                universityList: [],
+            }
+        },
+        methods:{
+            resisterCircle(){
+                setDoc(doc(db, "university", "0"), {
+                    number: this.number,
+                    dates: this.dates,
+                    places: this.places,
+                    text: this.text
+                });
+            }
+        },
+    }
+</script>
+
+<style></style>
