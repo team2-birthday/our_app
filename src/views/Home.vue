4f2ec2de1c96c86ec748dd4abee6f6007fa4a1a1
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2022-03-06 14:59:40 +0900

    一旦保存

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,19 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+  <RegisterCircle></RegisterCircle>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import RegisterCircle from '@/components/RegisterCircle.vue'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+    RegisterCircle
   }
 }
 </script>
