3f4cb328dc1f50ad103b96d12b7440b4afe69696
phasi <koumiyom4@gmail,com>
2022-03-07 02:10:31 +0900

    getdoc

---
+++ src/views/ShowCircle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{ circle_data }}
+  </div>
+</template>
+<script>
+import { collection, getDocs } from "firebase/firestore"
+import { db } from "../firebase.js"
+export default {
+  data() {
+    return {
+      circle_data: [],
+    }
+  },
+  methods: {},
+  created: async function () {
+    const snapshot = await getDocs(collection(db, "univ"))
+    await snapshot.forEach((doc) => {
+      this.circle_data.push({
+        id: doc.id,
+        ...doc.data(),
+      })
+    })
+  },
+}
+</script>
+<style></style>
