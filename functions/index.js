1cc37c7e43e8d2d9ee68bc4d8bfd5ad301001789
phasi <koumiyom4@gmail,com>
2022-03-27 17:41:40 +0900

    とりあえず完成！デバッグはこれから

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,21 @@
-const {
-  initializeApp,
-  applicationDefault,
-  cert,
-} = require("firebase-admin/app")
-const {
-  getFirestore,
-  Timestamp,
-  FieldValue,
-} = require("firebase-admin/firestore")
+const { initializeApp } = require("firebase-admin/app")
+const { getFirestore } = require("firebase-admin/firestore")
 const nodemailer = require("nodemailer")
 const functions = require("firebase-functions")
-const gmailEmail = "komiyo.univ334@gmail.com"
+// const { stringifyQuery } = require("vue-router")
 
-const docList = []
+import { mymail, authclientid, authsecretid, authrefreshToken } from "./my-data"
+const gmailEmail = mymail
+
+const today = new Date()
+const today_year = today.getFullYear()
+const today_date = today.getDate()
+let today_month = today.getMonth() + 1
+if (today_month < 10) {
+  today_month = "0" + String(today_month)
+}
+const today_string =
+  String(today_year) + String(today_month) + String(today_date)
 
 initializeApp()
 
@@ -21,40 +24,74 @@ const db = getFirestore()
 const auth = {
   type: "OAuth2",
   user: gmailEmail,
-  clientId:
-    "160429130531-e074odivtou9aikigcdk38ud43l14g87.apps.googleusercontent.com",
-  clientSecret: "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",
-  refreshToken:
-    "1//04red5sx8O5PwCgYIARAAGAQSNwF-L9IrfmFxeiwcz_5rzMBfmq5GR-X42yEMQGg7dSpHyWwKVntXnAEbAv-ydNSb-y-bft3nRLc",
+  clientId: authclientid,
+  clientSecret: authsecretid,
+  refreshToken: authrefreshToken,
 }
 
 const transport = {
   service: "gmail",
   auth,
 }
-
 const transporter = nodemailer.createTransport(transport)
-
-const sendTo = async function () {
+const getDocList = async function (docList) {
   const booksCollection = await db.collection("userData").get()
   booksCollection.forEach((doc) => {
-    console.log(doc.id, "=>", doc.data())
+    // console.log(doc.id, "=>", doc.data())
     docList.push(doc)
   })
 }
 
-const mailOptions = (email, date, circleName) => {
+// const get_activeCircle = async function () {
+//   const booksCollection = await db.collection("")
+// }
+
+const mailOptions = (email, circleName, date) => {
+  const sentence =
+    date + "にある、" + circleName + "の活動日が迫ってきました！！\n"
   return {
     from: gmailEmail,
     to: email,
     subject: "サークルの活動日が近づいてきました",
-    text: ,
+    text: sentence,
+  }
+}
+
+const doPost = () => {
+  let docList = []
+  let sendOptionList = []
+  getDocList(docList)
+  for (let i = 0; i < docList.length; i++) {
+    for (let j = 0; j < docList[i].newComerCircle.length; j++) {
+      let date = docList[i].newComerCircle[j].schedule[0].date
+      let cal_date = date.split("-").join("")
+      if (
+        Number(cal_date) - Number(today_string) > 0 &&
+        Number(cal_date) - Number(today_string) < 4
+      ) {
+        sendOptionList.push(
+          mailOptions(
+            docList[i].userMail,
+            docList[i].newComerCircle[j].circleName,
+            date
+          )
+        )
+      }
+    }
+  }
+  for (let i = 0; i < sendOptionList.length; i++) {
+    transporter.sendMail(sendOptionList[i], (err, response) => {
+      console.log(err || response)
+    })
   }
 }
 
 exports.sendMail = functions.https.onCall(async (data, context) => {
-  const x = await sendTo()
-  // transporter.sendMail(mailOptions, (err, response) => {
-  //   console.log(err || response)
-  // })
+  doPost()
 })
+
+exports.sendMail_auto = functions.pubsub
+  .schedule("every 1 days")
+  .onRun((context) => {
+    doPost()
+  })
