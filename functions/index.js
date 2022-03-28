1f62a9034912b3bab4c2c8f2addc9fec65cf056c
phasi <koumiyom4@gmail,com>
2022-03-28 22:10:09 +0900

    functionsの完成！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,19 @@
-const { initializeApp } = require("firebase-admin/app")
-const { getFirestore } = require("firebase-admin/firestore")
-const nodemailer = require("nodemailer")
+const { initializeApp } = require("firebase-admin/app") //Node.jsの形式なので、import,exportの形を使えない
+const { getFirestore } = require("firebase-admin/firestore") //exports.モジュール名 = データ
+const nodemailer = require("nodemailer") //const 名前 = require(モジュールがあるファイル名)
 const functions = require("firebase-functions")
-// const { stringifyQuery } = require("vue-router")
 
+//メアド、id,シークレットid,リフレッシュトークンをmy-dataファイル殻取ってきている
 const {
   mymail,
   authclientid,
   authsecretid,
   authrefreshToken,
 } = require("./my-data")
+
 const gmailEmail = mymail
 
-const today = new Date()
+const today = new Date() //今日の日付をとってくる
 const today_year = today.getFullYear()
 const today_date = today.getDate()
 let today_month = today.getMonth() + 1
@@ -20,13 +21,14 @@ if (today_month < 10) {
   today_month = "0" + String(today_month)
 }
 const today_string =
-  String(today_year) + String(today_month) + String(today_date)
+  String(today_year) + String(today_month) + String(today_date) //格納されている形式に合わせる
 
-initializeApp()
+initializeApp() //firebase admin SDKを有効にする
 
-const db = getFirestore()
+const db = getFirestore() //firebase firestoreのデータベースを取得する
 
 const auth = {
+  //認証情報(個人情報なのでgitignoreで隠したファイルから持ってきている)
   type: "OAuth2",
   user: gmailEmail,
   clientId: authclientid,
@@ -35,38 +37,57 @@ const auth = {
 }
 
 const transport = {
+  //送信形式の設定
   service: "gmail",
   auth,
 }
-const transporter = nodemailer.createTransport(transport)
+
+const transporter = nodemailer.createTransport(transport) //nodemailerを設定を反映して使用する
+
 const getDocList = async function () {
   const booksCollection = await db.collection("userData").get()
-  let sendOptionList = ["あたま"]
+  let sendOptionList = [] //送信対象となった人だけを格納するリストを作る
   booksCollection.forEach((doc) => {
     // console.log(doc.id, "=>", doc.data())
-    for (let j = 0; j < doc.newComerCircle.length(); j++) {
-      let date = doc.newComerCircle[j].schedule[0].date
-      let cal_date = date.split("-").join("")
-      if (
-        Number(cal_date) - Number(today_string) > 0 &&
-        Number(cal_date) - Number(today_string) < 4
-      ) {
-        sendOptionList.push(
-          mailOptions(doc.userMail, doc.newComerCircle[j].circleName, date)
-        )
+    // console.log(doc.data().newComerCircle[0])
+
+    for (let j = 0; j < doc.data().newComerCircle.length; j++) {
+      //.data()をつけないとデータの中身を読み取ってくれない(共通)
+      for (let k = 0; k < doc.data().newComerCircle[j].schedule.length; k++) {
+        let date = doc.data().newComerCircle[j].schedule[k].date
+        let cal_date = date.split("-").join("")
+        if (
+          //3日以内の時だけ通知を送信する
+          Number(cal_date) - Number(today_string) >= 0 &&
+          Number(cal_date) - Number(today_string) < 4
+        ) {
+          sendOptionList.push(
+            mailOptions(
+              doc.data().userMail,
+              doc.data().newComerCircle[j].circleName,
+              date
+            )
+          )
+        }
       }
     }
   })
-  console.log(sendOptionList)
+  // console.log(sendOptionList)
+  for (let i = 0; i < sendOptionList.length; i++) {
+    //各データにおいてメールを送信する
+    transporter.sendMail(sendOptionList[i], (err, response) => {
+      console.log(err || response)
+    })
+  }
 }
 
-// const get_activeCircle = async function () {
-//   const booksCollection = await db.collection("")
-// }
-
 const mailOptions = (email, circleName, date) => {
+  //送信情報を返す関数
   const sentence =
-    date + "にある、" + circleName + "の活動日が迫ってきました！！\n"
+    date +
+    "にある、" +
+    circleName +
+    "の活動日が迫ってきました！！\n日程、場所を確認しましょう！！"
   return {
     from: gmailEmail,
     to: email,
@@ -75,42 +96,14 @@ const mailOptions = (email, circleName, date) => {
   }
 }
 
-const doPost = () => {
-  let docList = []
-  let sendOptionList = []
-  getDocList(docList)
-  for (let i = 0; i < docList.length; i++) {
-    for (let j = 0; j < docList[i].newComerCircle.length; j++) {
-      let date = docList[i].newComerCircle[j].schedule[0].date
-      let cal_date = date.split("-").join("")
-      if (
-        Number(cal_date) - Number(today_string) > 0 &&
-        Number(cal_date) - Number(today_string) < 4
-      ) {
-        sendOptionList.push(
-          mailOptions(
-            docList[i].userMail,
-            docList[i].newComerCircle[j].circleName,
-            date
-          )
-        )
-      }
-    }
-  }
-  console.log(docList)
-  // for (let i = 0; i < sendOptionList.length; i++) {
-  //   transporter.sendMail(sendOptionList[i], (err, response) => {
-  //     console.log(err || response)
-  //   })
-  // }
-}
-
 exports.sendMail = functions.https.onCall(async (data, context) => {
+  //呼び出しで使える形式にしている
   getDocList()
 })
 
 exports.sendMail_auto = functions.pubsub
   .schedule("every 1 days")
   .onRun((context) => {
-    const x = doPost()
+    //1日ごとに送信されるようにしている
+    getDocList()
   })
